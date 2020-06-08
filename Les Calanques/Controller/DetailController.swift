6d5dffd4cdad058a1cffde4034eca6d49f5f440d
Sebastien SALIQUES <sebastien.saliques@gmail.com>
2020-06-08 17:09:14 +0200

    Ajout detailController

---
+++ Les Calanques/Controller/DetailController.swift
@@ -0,0 +1,47 @@
+//
+//  DetailController.swift
+//  Les Calanques
+//
+//  Created by Sébastien SALIQUES on 08/06/2020.
+//  Copyright © 2020 MassiveDynamic Ltd. All rights reserved.
+//
+
+import UIKit
+
+class DetailController: UIViewController {
+
+    @IBOutlet weak var calanqueIV: UIImageView!
+    @IBOutlet weak var nomEtDesc: UITextView!
+    
+    var calanqueRecue: Calanque?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let calanque = calanqueRecue else { return }
+        calanqueIV.image = calanque.image
+        
+        
+        let mutable = NSMutableAttributedString(string: calanque.nom + "\n", attributes: [
+            .foregroundColor: UIColor.red,
+            .font: UIFont.boldSystemFont(ofSize: 20)  ])
+        mutable.append(NSMutableAttributedString(string: calanque.desc, attributes: [
+            .font: UIFont.systemFont(ofSize: 17),
+            .foregroundColor: UIColor.darkGray]))
+        nomEtDesc.attributedText = mutable
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
